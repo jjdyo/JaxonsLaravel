bf0fbcc6cc1434ca95dbd6c911b26827db96fc92
Jaxon Dupre <63560773+jjdyo@users.noreply.github.com>
2025-03-19 12:29:39 -0400

    0.10.0 -- Roles pivot table

--- database/migrations/2025_03_19_162836_create_role_user_table.php
+++ database/migrations/2025_03_19_162836_create_role_user_table.php
@@ -4,22 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('role_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Links to users.id
+            $table->foreignId('role_id')->constrained()->onDelete('cascade'); // Links to roles.id
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('role_user');
