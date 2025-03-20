7c7099af22b81338039c53db33b184d7c35b43fa
Jaxon Dupre <63560773+jjdyo@users.noreply.github.com>
2025-03-20 12:08:23 -0400

    0.13.2 - Role extends Spatie - Not Model

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role as SpatieRole;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Role extends Model
 {
