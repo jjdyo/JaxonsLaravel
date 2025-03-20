b499872e4924da77957f779daff46134c3dd46f2
Jaxon Dupre <63560773+jjdyo@users.noreply.github.com>
2025-03-20 12:16:31 -0400

    0.13.4 - Add migration to give "roles" defined guard type (web/api)

--- database/migrations/2025_03_20_161520_add_guard_name_to_roles_table.php
+++ database/migrations/2025_03_20_161520_add_guard_name_to_roles_table.php
@@ -4,25 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('roles', function (Blueprint $table) {
-            //
+            $table->string('guard_name')->default('web')->after('name'); // ✅ Add guard_name with 'web' as default
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('roles', function (Blueprint $table) {
-            //
+            $table->dropColumn('guard_name');
         });
     }
 };
