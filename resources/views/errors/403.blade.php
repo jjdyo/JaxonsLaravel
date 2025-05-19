56aa12da7c665e3dee4c222d29133ebab64fcf39
Jaxon Dupre <63560773+jjdyo@users.noreply.github.com>
2025-05-19 08:41:57 -0400

    0.14.3 - Routes with Spaties permissions and 403 blade error

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>🚫 403 Forbidden</h1>
+        <p>You do not have permission to view this page.</p>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
-Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+Route::middleware(['auth', 'permission:view contact page url'])->group(function () {
+    Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+});
 Route::get('/user', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/user', [AuthController::class, 'processLogin'])->name('login.process');
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
