297f3ed1222ef099f39aaf779c6cd7ac81293dc9
Jaxon Dupre <63560773+jjdyo@users.noreply.github.com>
2025-05-19 12:30:50 -0400

    0.16.1: Correct layout extension uri

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,4 +1,4 @@
-@extends('layout')
+@extends('layouts.app')
 
 @section('title', 'Welcome')
 
@@ -7,11 +7,9 @@
         <h2>Welcome, {{ Auth::user()->name ?? Auth::user()->email }}!</h2>
         <p>This is your dashboard. Here you can view personalized info, upcoming stuff, etc.</p>
 
-        {{-- Example: Show user email --}}
         <p><strong>Email:</strong> {{ Auth::user()->email }}</p>
         <p><strong>Account creation date: {{ Auth::user()->created_at ? Auth::user()->created_at->format('F j, Y') : 'Unknown' }}
             </strong></p>
 
-        {{-- Add more personalized content here --}}
     </div>
 @endsection
