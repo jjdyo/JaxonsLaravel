8ab91d5f48b08790f20859b7ca2a9fd6030bddc9
Jaxon Dupre <63560773+jjdyo@users.noreply.github.com>
2025-05-19 08:47:53 -0400

    0.14.5 - Middleware check definitions for spaties permissions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
