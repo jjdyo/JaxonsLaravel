20ce191f8aa932c9ae50a887ce8873d1489ddcc5
Jaxon Dupre <63560773+jjdyo@users.noreply.github.com>
2025-07-31 17:11:57 -0400

    0.42.9: cors.php update

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
+    'allowed_origins' => [
+        'http://192.168.3.175:5173',
+    ],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
