f290a40547ba23d7bcd5e6f54a4856b2d6ff0a34
Jaxon Dupre <63560773+jjdyo@users.noreply.github.com>
2025-09-04 10:52:15 -0400

    0.62.9: Include Str

--- app/Http/Controllers/SystemLogsController.php
+++ app/Http/Controllers/SystemLogsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class SystemLogsController extends Controller
 {
