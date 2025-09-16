58e792d26e2c1861e6144082e73da134bf8e268e
Jaxon Dupre <63560773+jjdyo@users.noreply.github.com>
2025-09-16 06:23:03 -0400

    0.75.24: Remote throttle for reset password

--- routes/web.php
+++ routes/web.php
@@ -52,8 +52,8 @@ Route::middleware('guest')->group(function () {
     Route::get('/forgot-password', [AuthController::class, 'showForgotPasswordForm'])->name('password.request');
     Route::post('/forgot-password', [AuthController::class, 'sendResetLinkEmail'])->name('password.email');
 
-    Route::middleware('throttle:5,1')->group(function () {
-        Route::get('/reset-password/{token?}', [AuthController::class, 'showResetPasswordForm'])
+    Route::group([], function () {
+        Route::get('/reset-password/{token}', [AuthController::class, 'showResetPasswordForm'])
             ->name('password.reset');
 
         Route::post('/reset-password', [AuthController::class, 'resetPassword'])->name('password.update');
