358736ef5de17712cead548a3bf640a6bbf9f994
Jaxon Dupre <63560773+jjdyo@users.noreply.github.com>
2025-09-17 04:17:17 -0400

    0.79.0: Add index to frequently queried user model columns

--- database/migrations/2023_07_16_000013_create_users_table.php
+++ database/migrations/2023_07_16_000013_create_users_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable()->index();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->index('created_at');
         });
     }
 
